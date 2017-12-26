99fc875a3b96622f6b6456810a059caf2c6afe71
Islam Mansour <islam.alam.aerospace@gmail.com>
2017-12-27 00:36:41 +0100

    Update API Version
    After updating api version this error will not be showed anymore 
    UserWarning: Please supply API version (YYYY-MM-DD) as CB-VERSION header (https://developers.coinbase.com/api#versioning)
      warnings.warn(message, UserWarning)

--- coinbase/wallet/client.py
+++ coinbase/wallet/client.py
@@ -65,7 +65,7 @@ class Client(object):
   VERIFY_SSL = True
 
   BASE_API_URI = 'https://api.coinbase.com/'
-  API_VERSION = '2016-02-18'
+  API_VERSION = '2017-12-17'
 
   cached_callback_public_key = None
 
